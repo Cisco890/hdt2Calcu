421285916e9e6ef899f804d77512543fff4ed9cf
Juan Francisco Martínez <jfmartinez890@gmail.com>
2024-01-22 16:45:54 -0600

    Interfaz 1 de la calculadora

---
+++ ICalculadora.java
@@ -0,0 +1,12 @@
+/** Anthony Lou Shwank 23410/Juan Francisco Martínez 23617
+  * Icalculadora
+ 
+  * @param   no_hay
+  * @throws Interfaz creada por todos en la clase
+  */
+  interface Icalculadora{
+    int suma(int op1, int op2);
+    int resta(int op1, int op2);
+    int multiplicacion(int op1, int op2);
+    int division(int op1, int op2);
+  }
