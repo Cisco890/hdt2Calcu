1402adb7f0a2ad54dbd134a3489977c4bb8a3bb7
Anthony Lou <anthonylouschwank@gmail.com>
2024-01-22 18:05:18 -0600

    Creacion de la clase que tiene metodos de calculo

---
+++ Calculador.java
@@ -0,0 +1,20 @@
+public class Calculador implements Icalculadora {
+
+    public int suma(int op1, int op2) {
+        op1 + op2 = int value;
+
+       
+    }
+    public int resta(int op1, int op2) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'resta'");
+    }
+    public int multiplicacion(int op1, int op2) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'multiplicacion'");
+    }
+    public int division(int op1, int op2) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'division'");
+    }
+}
